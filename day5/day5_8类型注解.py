9e39f49196de33390f9e4ec071984a32dbebbed0
BLFOREVER <3026687133@qq.com>
2025-07-03 10:38:38 +0800

    7.2

---
+++ day5/day5_8类型注解.py
@@ -0,0 +1,23 @@
+import random
+
+var1: int = 5
+var2: bool = True
+var3: str = "123"
+
+
+class Student:
+    pass
+
+
+stu: Student = Student()
+
+my_list: list = [1, 2, 3]
+my_tuple: tuple = (1, 2, 3)
+my_dict: dict = {"苹果": 1999}
+
+my_list: list[int] = [1, 2, 3]
+my_tuple: tuple[int] = (1, 2, 3)
+
+# 注释注解
+
+var_1 = random.randint(1, 10)  # type:int
